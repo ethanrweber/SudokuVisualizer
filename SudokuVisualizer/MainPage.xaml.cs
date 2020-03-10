359a51aff940db2bd7dae164ada1348fa53f3ec8
Ethan Weber <Ethan.Weber@molex.com>
2020-03-10 17:23:05 -0500

    Init Project
    - Create UWP project
    - Import self-made Sudoku class from my ProjectEuler repo
    - Create Sudoku grid on screen

---
+++ SudokuVisualizer/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SudokuVisualizer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Sudoku puzzle;
+        private Grid grid;
+
+        private const int n = 9;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            puzzle = new Sudoku(new int[n, n]);
+
+
+            makeSudoku();
+            makeGrid();
+        }
+
+        private void makeGrid()
+        {
+            grid = this.FindName("rootGrid") as Grid;
+
+            if(grid == null)
+                throw new Exception("Could not find root grid element");
+
+            for (int k = 0; k < n * n; k++)
+            {
+                int i = k / n, j = k % n;
+                TextBox tb = new TextBox();
+                tb.Name = $"{i}{j}";
+                tb.Text = puzzle[i, j] == 0 ? "" : $"{puzzle[i, j]}";
+                tb.IsReadOnly = true;
+                tb.FontSize = 40;
+                tb.TextAlignment = TextAlignment.Center;
+                tb.BorderThickness = new Thickness(1);
+                grid.Children.Add(tb);
+                Grid.SetRow(tb, i);
+                Grid.SetColumn(tb, j);
+            }
+        }
+
+        private void makeSudoku()
+        {
+            string values = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
+            for (int i = 0; i < values.Length; i++)
+                puzzle[i / n, i % n] = values[i] - '0';
+        }
+    }
+}
---
+++ SudokuVisualizer/Sudoku.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuVisualizer
+{
+    public class Sudoku
+    {
+        private const int n = 9;
+        public int[,] Grid { get; set; }
+
+        public Sudoku(int[,] grid)
+        {
+            if (grid.GetLength(0) != n || grid.GetLength(1) != n)
+                throw new Exception("grid must be 9x9");
+
+            Grid = grid;
+        }
+
+        public int this[int i, int j]
+        {
+            get => Grid[i, j];
+            set => Grid[i, j] = value;
+        }
+
+    }
+
+    public static class SudokuExtensions
+    {
+        private const int n = 9;
+
+        public static bool isAnswer(this Sudoku s)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                // build row and column;
+                int[] row = new int[n], col = new int[n];
+                for (int j = 0; j < n; j++)
+                {
+                    row[j] = s.Grid[i, j];
+                    col[j] = s.Grid[j, i];
+                }
+
+                // verify rows and columns
+                if (!verifyRowCol(row) || !verifyRowCol(col))
+                    return false;
+            }
+
+            // generate boxes
+            int[,] box1 = new int[3, 3];
+            int[,] box2 = new int[3, 3];
+            int[,] box3 = new int[3, 3];
+            int[,] box4 = new int[3, 3];
+            int[,] box5 = new int[3, 3];
+            int[,] box6 = new int[3, 3];
+            int[,] box7 = new int[3, 3];
+            int[,] box8 = new int[3, 3];
+            int[,] box9 = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    box1[i, j] = s[i, j];
+                    box4[i, j] = s[i + 3, j];
+                    box7[i, j] = s[i + 6, j];
+                }
+
+                for (int j = 3; j < 6; j++)
+                {
+                    box2[i, j - 3] = s[i, j];
+                    box5[i, j - 3] = s[i + 3, j];
+                    box8[i, j - 3] = s[i + 6, j];
+                }
+
+                for (int j = 6; j < 9; j++)
+                {
+                    box3[i, j - 6] = s[i, j];
+                    box6[i, j - 6] = s[i + 3, j];
+                    box9[i, j - 6] = s[i + 6, j];
+                }
+            }
+
+            // verify boxes
+            List<int[,]> boxes = new List<int[,]>
+                { box1, box2, box3, box4, box5, box6, box7, box8, box9 };
+
+            return boxes.All(verifyBox);
+        }
+
+        public static bool solveSudoku(this Sudoku board)
+        {
+            int row = -1, col = -1;
+            bool isEmpty = true;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        isEmpty = false;
+                        break;
+                    }
+                }
+
+                if (!isEmpty)
+                    break;
+            }
+
+            if (isEmpty)
+                return true;
+
+            for (int num = 1; num <= n; num++)
+            {
+                if (isSafe(board, row, col, num))
+                {
+                    board[row, col] = num;
+                    if (solveSudoku(board))
+                        return true;
+                    else
+                        board[row, col] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool isSafe(Sudoku board, int row, int col, int num)
+        {
+            for (int d = 0; d < n; d++)
+                if (board[row, d] == num)
+                    return false;
+            for (int r = 0; r < n; r++)
+                if (board[r, col] == num)
+                    return false;
+            int sqrt = 3;
+            int boxRowStart = row - row % sqrt;
+            int boxColStart = col - col % sqrt;
+
+            for (int r = boxRowStart; r < boxRowStart + sqrt; r++)
+                for (int d = boxColStart; d < boxColStart + sqrt; d++)
+                    if (board[r, d] == num)
+                        return false;
+            return true;
+        }
+
+        private static HashSet<int> answerArr = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        private static bool verifyRowCol(int[] r) => r.ToHashSet().SetEquals(answerArr);
+
+        private static bool verifyBox(int[,] box)
+        {
+            HashSet<int> vals = new HashSet<int>();
+            foreach (int i in box)
+                vals.Add(i);
+            return vals.SetEquals(answerArr);
+        }
+
+
+    }
+}
